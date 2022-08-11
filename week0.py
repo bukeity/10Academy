2c84af3ea0a44d6d55f66451be968d3d596191a6
bukeity <kiobbomichelle@gmail.com>
2022-08-11 17:08:22 +0300

    n/a

---
+++ week0.py
@@ -0,0 +1,76 @@
+import pandas as pd
+
+
+def drop_unwanted_column(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    remove rows that have column names. This error originated from
+    the data collection stage.
+    """
+    unwanted_rows = df[df['retweet_count'] == 'retweet_count'].index
+    df.drop(unwanted_rows, inplace=True)
+    df = df[df['polarity'] != 'polarity']
+
+    return df
+
+
+def drop_duplicate(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    drop duplicate rows
+    """
+    df.drop_duplicates(['screen_name', 'original_text', 'created_at'], keep="first")
+
+    return df
+
+
+def convert_to_datetime(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    convert column to datetime
+    """
+    df['created_at'] = pd.to_datetime(df['created_at'])
+
+    df = df[df['created_at'] >= '2020-12-31']
+
+    return df
+
+
+def convert_to_numbers(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    convert columns like polarity, subjectivity, retweet_count
+    favorite_count etc to numbers
+    """
+
+    df['subjectivity'] = pd.to_numeric(df['subjectivity'])
+
+    df['retweet_count'] = pd.to_numeric(df['retweet_count'])
+
+    df['screen_count'] = pd.to_numeric(df['screen_count'])
+
+    df['friends_count'] = pd.to_numeric(df['friends_count'])
+
+    df['followers_count'] = pd.to_numeric(df['followers_count'])
+
+    df['favorite_count'] = pd.to_numeric(df['favorite_count'])
+
+    df['polarity'] = pd.to_numeric(df['polarity'])
+
+    return df
+
+
+def remove_non_english_tweets(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    remove non english tweets from lang
+    """
+
+    df = df[df['lang'] == 'en']
+
+    return df
+
+
+class CleanTweets:
+    """
+    The PEP8 Standard AMAZING!!!
+    """
+
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+        print('Automation in Action...!!!')
